fe977d32fff8a60445bcefebc3a8a10880eb01a9
Muwaffaq Imam <muwaffaqimam@gmail.com>
2021-08-22 10:29:37 +0300

    --finishing process in background

--- lib/mainStreams.dart
+++ lib/mainStreams.dart
@@ -2,8 +2,11 @@ import 'dart:async';
 import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:image_searcher_ai/ai/aiHelper.dart';
 import 'package:image_searcher_ai/data/classes.dart';
 import 'package:photo_manager/photo_manager.dart';
@@ -18,8 +21,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'serif'),
-      home: MyHomePage(title: 'Smart Gallery'),
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+        accentColor: Colors.deepOrangeAccent,
+        fontFamily: 'serif',
+      ),
+      home: MyHomePage(title: 'الألبوم الذكي'),
     );
   }
 }
@@ -39,34 +46,64 @@ class _MyHomePageState extends State<MyHomePage> {
   late List<SmartGallery> smartGalleryCopy = [];
   late TextEditingController searchController;
   late StreamController<SmartGallery> streamController;
+  late StreamController<String> streamControllerString;
+  int processedImages = 0;
+  int totalImages = 0;
+  bool isDoneFetching = false;
   final _onDeviceTranslator = GoogleMlKit.nlp.onDeviceTranslator(
       sourceLanguage: TranslateLanguage.ENGLISH,
       targetLanguage: TranslateLanguage.ARABIC);
+
+  late ImagePicker _imagePicker;
 
   @override
   void initState() {
     // TODO: implement initState
     requestPermission();
-    _fetchImages();
+    computeImage();
     searchController = new TextEditingController();
     streamController = new StreamController<SmartGallery>();
     streamController.stream.listen((smart) {
+      smartGallery.add(smart);
+      smartGalleryCopy.add(smart);
       setState(() {
-        smartGallery.add(smart);
-        smartGalleryCopy.add(smart);
+        processedImages++;
       });
     }).onDone(() {
-      print("done");
+      print("on done is called");
+      setState(() {
+
+      });
     });
+    _imagePicker = ImagePicker();
     super.initState();
   }
 
   @override
   void dispose() {
     streamController.close();
+    streamControllerString.close();
     _onDeviceTranslator.close();
     searchController.dispose();
     super.dispose();
+  }
+
+  Future _getImage() async {
+    final pickedFile =
+        await _imagePicker?.getImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      var imageActualFile = File(pickedFile.path);
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (_) => ExtractText(
+            imageActualFile: imageActualFile,
+          ),
+        ),
+      );
+    } else {
+      print('No image selected.');
+    }
   }
 
   @override
@@ -74,40 +111,53 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.inbox_sharp))],
+        actions: [IconButton(onPressed: () {computeImage();}, icon: Icon(Icons.inbox_sharp))],
       ),
-      body: Column(
-        children: [
-          TextField(
-            onChanged: (text) {
-              text = text.trim();
-              if (text.isNotEmpty)
-                search(text);
-              else
-                setState(() {
-                  clearSearch();
-                });
-            },
-            decoration: new InputDecoration(
-                prefixIcon: IconButton(
-                  onPressed: () {
-                    setState(() {
-                      clearSearch();
-                    });
-                  },
-                  icon: Icon(Icons.close),
-                ),
-                hintText: "Search here..",
-                hintStyle: new TextStyle(color: Colors.white)),
-            controller: searchController,
-          ),
-          Expanded(child: buildGrid(smartGallery: smartGallery)),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _fetchImages,
-        child: Icon(Icons.add),
+      body: isDoneFetching
+          ? buildMainBody()
+          : Text('we are processing $processedImages / $totalImages'),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          _getImage();
+        },
+        icon: Icon(Icons.image),
+        label: Text('إستخرج أي نص من صورة'),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Column buildMainBody() {
+    return Column(
+      children: [
+        TextField(
+          onChanged: (text) {
+            text = text.trim();
+            if (text.isNotEmpty)
+              search(text);
+            else
+              setState(() {
+                clearSearch();
+              });
+          },
+          decoration: new InputDecoration(
+              filled: true,
+              suffixIcon: IconButton(
+                onPressed: () {
+                  setState(() {
+                    clearSearch();
+                  });
+                },
+                icon: Icon(Icons.close),
+              ),
+              prefixIcon: Icon(Icons.image_search_rounded),
+              hintText: "إبحث في الصور",
+              hintTextDirection: TextDirection.rtl,
+              hintStyle: new TextStyle(color: Colors.grey)),
+          textDirection: TextDirection.rtl,
+          controller: searchController,
+        ),
+        Expanded(child: buildGrid(smartGallery: smartGallery)),
+      ],
     );
   }
 
@@ -135,20 +185,25 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  GridView buildGrid({required List<SmartGallery> smartGallery}) {
-    return GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-        ),
-        itemCount: smartGallery.length,
-        itemBuilder: (_, index) {
-          return AssetThumbnail(
-            asset: smartGallery[index].images,
-            imageLabels: smartGallery[index].labels,
-            imageBytes: smartGallery[index].imageByte,
-            translated: smartGallery[index].translatedLabels,
-          );
-        });
+  Widget buildGrid({required List<SmartGallery> smartGallery}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 4.0),
+      child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 2,
+            crossAxisSpacing: 2,
+          ),
+          itemCount: smartGallery.length,
+          itemBuilder: (_, index) {
+            return AssetThumbnail(
+              asset: smartGallery[index].images,
+              imageLabels: smartGallery[index].labels,
+              imageBytes: smartGallery[index].imageByte,
+              translated: smartGallery[index].translatedLabels,
+            );
+          }),
+    );
   }
 
   void requestPermission() async {
@@ -157,13 +212,18 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  computeImage() async {
+    await compute(_fetchImages(), 2);
+  }
+
   _fetchImages() async {
     await TranslatorHelper().checkModels();
     final albums = await PhotoManager.getAssetPathList(
-        onlyAll: true, type: RequestType.image);
+        onlyAll: true, hasAll: true, type: RequestType.image);
     final recentAlbum = albums.first;
-    // Now that we got the album, fetch all the assets it contains
-    var assets = await recentAlbum.getAssetListPaged(0, 10);
+    totalImages = recentAlbum.assetCount;
+    var assets =
+        await recentAlbum.getAssetListRange(start: 0, end: 10);
     await processTheAssets(assets);
   }
 
@@ -178,6 +238,9 @@ class _MyHomePageState extends State<MyHomePage> {
           imageByte: bytes,
           translatedLabels: translate));
     }
+    setState(() {
+      isDoneFetching = true;
+    });
   }
 
   Future<List<TranslatedImageLabel>> translateLabels(
@@ -240,40 +303,43 @@ class AssetThumbnail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // We're using a FutureBuilder since thumbData is a future
-    return InkWell(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (_) => ImageScreen(
-              imageFile: asset.file,
-              imgLabels: imageLabels,
-              translateLabel: translated,
+    return Card(
+      elevation: 2,
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => ImageScreen(
+                imageFile: asset.file,
+                imgLabels: imageLabels,
+                translateLabel: translated,
+              ),
             ),
-          ),
-        );
-      },
-      child: Stack(
-        children: [
-          // Wrap the image in a Positioned.fill to fill the space
-          Positioned.fill(
-            child: Image.memory(imageBytes!, fit: BoxFit.cover),
-          ),
-          // Display a Play icon if the asset is a video
-          Container(
-            color: Colors.black26,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  // ' ${imageLabels.isEmpty ? 'no labels' : imageLabels.first.label} | ${translated.first.translatedLabel} ',
-                  ' ${translated.first.translatedLabel} ',
-                  style: TextStyle(color: Colors.white, fontSize: 18),
-                ),
-              ],
+          );
+        },
+        child: Stack(
+          children: [
+            // Wrap the image in a Positioned.fill to fill the space
+            Positioned.fill(
+              child: Image.memory(imageBytes!, fit: BoxFit.cover),
             ),
-          ),
-        ],
+            // Display a Play icon if the asset is a video
+            Container(
+              color: Colors.black26,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    // ' ${imageLabels.isEmpty ? 'no labels' : imageLabels.first.label} | ${translated.first.translatedLabel} ',
+                    ' ${translated.first.translatedLabel} ',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -308,36 +374,47 @@ class _ImageScreenState extends State<ImageScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton.extended(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (_) => ExtractText(
-                  imageActualFile: imageActualFile,
-                ),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => ExtractText(
+                imageActualFile: imageActualFile,
               ),
-            );
-          },
-          label: Text('إستخراج النص')),
+            ),
+          );
+        },
+        label: Text('إستخراج النص من الصورة'),
+        icon: Icon(Icons.text_fields_rounded),
+      ),
       body: Stack(
         fit: StackFit.expand,
         children: [
           Container(
-              color: Colors.black,
-              alignment: Alignment.center,
-              child: imageActualFile != null
-                  ? Image.file(imageActualFile!)
-                  : FutureBuilder<File?>(
-                      future: imageFile,
-                      builder: (_, snapshot) {
-                        final file = snapshot.data;
-                        if (file == null) return Text("Error Loading Image");
-                        setState(() {
-                          imageActualFile = file;
-                        });
-                        return CircularProgressIndicator();
-                      },
-                    )),
+            color: Colors.black,
+            alignment: Alignment.center,
+            child: imageActualFile != null
+                ? Image.file(imageActualFile!)
+                : FutureBuilder<File?>(
+                    // Initialize FlutterFire:
+                    future: imageFile,
+                    builder: (context, snapshot) {
+                      // Check for errors
+                      if (snapshot.hasError) {
+                        return Text('${snapshot.error}');
+                      }
+
+                      // Once complete, show your application
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        imageActualFile = snapshot.data;
+                        return Image.file(imageActualFile!);
+                      }
+
+                      // Otherwise, show something whilst waiting for initialization to complete
+                      return Center(child: CircularProgressIndicator());
+                    },
+                  ),
+          ),
           buildList()
         ],
       ),
@@ -350,29 +427,29 @@ class _ImageScreenState extends State<ImageScreen> {
         initialChildSize: 0.25,
         builder: (BuildContext context, ScrollController scrollController) {
           return Container(
-              color: Colors.black26,
-              child: ListView.builder(
-                controller: scrollController,
-                itemCount: widget.imgLabels.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    subtitle: Text(
-                      ' ${widget.imgLabels[index].label}  ${(widget.imgLabels[index].confidence * 100).truncate()}%',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                      ),
+            color: Colors.black26,
+            child: ListView.builder(
+              controller: scrollController,
+              itemCount: widget.imgLabels.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  subtitle: Text(
+                    ' ${widget.imgLabels[index].label}  ${(widget.imgLabels[index].confidence * 100).truncate()}%',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
                     ),
-                    title: Text(
-                      '${widget.translateLabel[index].translatedLabel}',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                      ),
+                  ),
+                  title: Text(
+                    '${widget.translateLabel[index].translatedLabel}',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
                     ),
-                  );
-                },
-              ),
+                  ),
+                );
+              },
+            ),
           );
         },
       ),
@@ -400,7 +477,9 @@ class ExtractText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('إستخراج النص من الصور'),
+      ),
       body: FutureBuilder(
         // Initialize FlutterFire:
         future: recogniseText(imageActualFile),
@@ -411,28 +490,22 @@ class ExtractText extends StatelessWidget {
           }
           if (snapshot.connectionState == ConnectionState.done) {
             List<TextBlock> extractedTexts = snapshot.data as List<TextBlock>;
-            return ListView.builder(
-                itemCount: extractedTexts.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    subtitle: Text(
-                      ' ${extractedTexts[index].recognizedLanguages}',
-                      style: TextStyle(
-                        fontSize: 12,
-                      ),
-                    ),
-                    title: Text(
-                      ' ${extractedTexts[index].text}',
-                      style: TextStyle(
-                        fontSize: 16,
-                      ),
-                    ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.copy),
-                      onPressed: () {},
-                    ),
-                  );
-                });
+
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Card(
+                    elevation: 4,
+                    color: Colors.black,
+                    child: Image.file(
+                      imageActualFile!,
+                      height: 300,
+                    )),
+                extractedTexts.isEmpty
+                    ? Expanded(child: emptyView())
+                    : Expanded(child: buildListView(extractedTexts)),
+              ],
+            );
           }
 
           // Otherwise, show something whilst waiting for initialization to complete
@@ -442,6 +515,35 @@ class ExtractText extends StatelessWidget {
     );
   }
 
+  ListView buildListView(List<TextBlock> extractedTexts) {
+    return ListView.builder(
+        itemCount: extractedTexts.length,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              ListTile(
+                title: Text(
+                  ' ${extractedTexts[index].text}',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                trailing: IconButton(
+                  icon: Icon(
+                    Icons.copy,
+                    color: Colors.blue,
+                  ),
+                  onPressed: () {
+                    _copyToClipboard(context, extractedTexts[index].text);
+                  },
+                ),
+              ),
+              Divider(),
+            ],
+          );
+        });
+  }
+
   Future<List<TextBlock>> recogniseText(File? imageActualFile) async {
     TextDetector textDetector = GoogleMlKit.vision.textDetector();
     InputImage inputImage = InputImage.fromFile(imageActualFile!);
@@ -449,5 +551,33 @@ class ExtractText extends StatelessWidget {
     return recognisedText.blocks
         .where((element) => !element.recognizedLanguages.contains("und"))
         .toList();
+  }
+
+  Future<void> _copyToClipboard(BuildContext context, String text) async {
+    await Clipboard.setData(ClipboardData(text: text));
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('تم نسخ النص'),
+      ),
+    );
+  }
+
+  Widget emptyView() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.format_color_text,
+            color: Colors.blue,
+            size: 100,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text("لا يوجد نص تم التعرف عليه في الصورة"),
+          ),
+        ],
+      ),
+    );
   }
 }
